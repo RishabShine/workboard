7de6a4b030d4abfdd5ea22e0d3948f307b8f2fa6
Rishab Shine <shinerishab@gmail.com>
2026-01-24 16:01:24 -0500

    created backend project and docker-compose

---
+++ workboard-api/src/main/java/com/rishab/workboard/api/WorkboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.rishab.workboard.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkboardApiApplication.class, args);
+    }
+
+}
